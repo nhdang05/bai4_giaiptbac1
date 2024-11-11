8a2473798ca0c5a5def5329e65d89f766a582c18
nhdang05 <nguenhaidang1162005@gmail.com>
2024-11-11 13:47:04 +0700

    Add project files.

---
+++ bai4_giaiptbac1/bai4_giaiptbac1.cpp
@@ -0,0 +1,31 @@
+// bai4_giaiptbac1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    float x;
+    cout << "UNG DUNG GIAI PHUONG BAC I\n";
+    cout << "Nhap a:"; cin >> a;
+    cout << "Nhap b:"; cin >> b;
+    if (a == 0)
+    {
+        if (a == 0)
+        {
+            cout << "PT dung voi moi x ~ vo so nghiem\n";
+        }
+        else
+        {
+            cout << "PT vo nghiem\n";
+        }
+    }
+
+    else
+    {
+        x = (float)-b / a;
+        cout << "ket qua x =" << x << endl;
+    }
+    return 0;
+}
